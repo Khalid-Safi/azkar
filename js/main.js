fde4fa9a454769b1b4a4996ff16cd58fdcc1ca9a
minimap <137304153+Khalid-Safi@users.noreply.github.com>
2024-05-04 23:00:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,24 +27,23 @@ function alazkar(){
 }
 alazkar();
 
-// document.querySelectorAll(".azkar-title").forEach(function(zkr){
-//     zkr.onclick
-// })
+
 function chooseZkr(elemid){
-    
+    id = elemid
     fetch("./JSON/adhkar.json").then((res)=>res.json())
     .then(function(data){
         title.innerHTML = `
-        <h1 class="text-center py-4 text-white" id="title">${data[elemid].category}</h1>
+        <h1 class="text-center py-4 text-white" id="title">${data[id].category}</h1>
         
         `
+        
         azkarAlsabah.innerHTML = ' ';
-        for(let i= 0; i< data[elemid].array.length; i++){
-            azkarAlsabah.innerHTML =` <div class="card" style="width: 300px;">
+        for(let i= 0; i < data[id].array.length; i++){
+            azkarAlsabah.innerHTML +=` <div class="card" style="width: 300px;">
             <div class="card-body">
-              <p class="fs-3">${data[elemid].array[i].text}</p>
+              <p class="fs-3">${data[id].array[i].text}</p>
             </div>
-            <span class="text-center fs-2 bg-warning">${data[elemid].array[i].count}</span>
+            <span class="text-center fs-2 bg-warning">${data[id].array[i].count}</span>
           </div>
           `;
         }
